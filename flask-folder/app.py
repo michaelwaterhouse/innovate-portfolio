f30e1b5b45a4d4563b6a9f7a663ada5aa5e1870d
Michael Waterhouse <michaelwaterhouse1@googlemail.com>
2022-03-25 11:56:35 +0000

    future page created and linked to base

---
+++ flask-folder/app.py
@@ -0,0 +1,53 @@
+######################################
+#* FLASK APPLICATION
+######################################
+
+# extensions that allow specific classes and functions.
+from flask import Flask, Blueprint, render_template, url_for, redirect
+
+# a 'blueprint' of the website structure.
+views = Blueprint(__name__, "views")
+
+# the website is defined as a flask app and the url prefix is set to ""
+app = Flask(__name__)
+app.register_blueprint(views, url_prefix = "/")
+
+#########################################
+#* APPLICATION PAGES
+#########################################
+
+
+# this is how  the homepage is accessed.
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/michael")
+def michael_page():
+    return render_template("michael.html", user_name = "Michael", favourite_thing = "music")
+
+@app.route("/bobby")
+def bobby_page():
+    return render_template("bobby.html", user_name = "Bobby the Sausage Dog", favourite_thing = "Tennis ball")
+
+#########################################
+
+app.route("/home")
+def home_redirect():
+    return redirect (url_for("home"))
+
+##########################################
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+###########################################
+
+#debugging is activated and the project is set to hosted on port 8000 (debugging should only be ysed in testing!)
+
+if __name__ == "__main__":
+    app.run(debug = True, port = 8000)
+
+##########################################
